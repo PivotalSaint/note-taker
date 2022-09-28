fdf6875385812d8376837a86cae49ce11758a4a9
PivotalSaint <licensedmt@att.net>
2022-09-28 17:18:14 -0500

    functionality to note landing page done

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -3,7 +3,7 @@ const { filterByQuery, findById, createNewNotes, validateNotes } = require('../.
 const { db } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
-    let results = notes;
+    let results = notesList;
     if (req.query) {
       results = filterByQuery(req.query, results);
     }
@@ -21,10 +21,10 @@ router.get('/notes', (req, res) => {
   
   router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
+    req.body.id = noteList.length.toString();
   
     if (!validateNotes(req.body)) {
-      res.status(400).send('The note is not properly formatted.');
+      res.status(400).send('The notes are not properly formatted.');
     } else {
       const notes = createNewNotes(req.body, notes);
       res.json(notes);
